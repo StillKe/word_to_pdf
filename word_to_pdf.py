2569232db8067dab9bed37bf043fd7a9540649e7
mamasb <pkpipimwangi@gmail.com>
2024-05-05 09:45:44 +0300

    Work in progress

---
+++ word_to_pdf.py
@@ -0,0 +1,13 @@
+from docx2pdf import convert
+
+def word_to_pdf(word_path, output_path):
+    try:
+        convert(word_path, output_path)
+        print(f"Word document converted to PDF: {output_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    word_path = "output.docx"  # Replace with the actual path to your Word document
+    output_path = "output.pdf"  # Replace with the desired output path for the PDF
+    word_to_pdf(word_path, output_path)
